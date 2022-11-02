7dd04fb9d68c35e5baf1f93ff0a6efd74ecfec8b
ceramicSoda <dmch.d89@gmail.com>
2022-11-02 04:03:47 +0200

    minor awesome update

--- .config/awesome/___test.lua
+++ .config/awesome/___test.lua
@@ -4,3 +4,18 @@ local hui = {
     {modkey, "Alt", "Right"},
     {modkey, "", "Up"},
 }
+
+function dump(o)
+    if type(o) == 'table' then
+       local s = '{ '
+       for k,v in pairs(o) do
+          if type(k) ~= 'number' then k = '"'..k..'"' end
+          s = s .. dump(v) .. ','
+       end
+       return s .. '} '
+    else
+       return tostring(o)
+    end
+ end
+
+ print(dump(hui))
